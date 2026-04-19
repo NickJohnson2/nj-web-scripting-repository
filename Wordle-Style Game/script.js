f2afe09be8636f697532f3f81d30b4a5f2d51a50
NickJohnson2 <njohnson2@lakelandcollege.edu>
2026-04-19 18:26:16 +0000

    modified script

--- Wordle-Style Game/script.js
+++ Wordle-Style Game/script.js
@@ -2,7 +2,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "MERCY"
+let rightGuessString = "mercy"
 console.log(rightGuessString)
 
 function initBoard() {
